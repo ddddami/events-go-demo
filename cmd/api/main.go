5663a6a6815411e300948bbe8654a641ff93403a
Damilola O. <82976159+ddddami@users.noreply.github.com>
2025-04-25 08:29:36 +0100

    Add sqlite db

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 
 	"github.com/gin-gonic/gin"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 const version = "1.0.0"
@@ -12,8 +14,54 @@ func main() {
 	addr := flag.String("addr", ":4000", "HTTP server port")
 	flag.Parse()
 
+	db, err := initDB()
+	if err != nil {
+		panic(err)
+	}
+
+	createTables()
+
+	defer db.Close()
+
 	server := gin.Default()
 
 	registerRoutes(server)
 	server.Run(*addr)
+}
+
+var DB *sql.DB
+
+func initDB() (*sql.DB, error) {
+	var err error
+	DB, err = sql.Open("sqlite3", "events.db")
+	if err != nil {
+		return nil, err
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		DB.Close()
+		return nil, err
+	}
+
+	DB.SetMaxOpenConns(10)
+	DB.SetMaxIdleConns(5)
+	return DB, nil
+}
+
+func createTables() {
+	stmt := `
+CREATE TABLE IF NOT EXISTS events (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	name TEXT NOT NULL,
+	description TEXT NOT NULL,
+	location TEXT NOT NULL,
+	dateTime TEXT NOT NULL,
+	userId INTEGER
+)
+`
+	_, err := DB.Exec(stmt)
+	if err != nil {
+		panic("Error working with db")
+	}
 }
